406786d00be3cf8ce5e8a3c352f0ecec0f09424d
Opa Reklama <agency@opareklama.com>
2026-07-15 12:57:00 +0600

    Bump version to 1.1.0 for testing updater

--- order-shield.php
+++ order-shield.php
@@ -3,7 +3,7 @@
  * Plugin Name: Order Shield
  * Plugin URI: https://github.com/hasibulhasansakib/order-shield
  * Description: A production-ready, open-source WooCommerce fraud prevention and order protection system.
- * Version: 1.0.9
+ * Version: 1.1.0
  * Author: Hasibul Hasan Sakib
  * Author URI: https://hasibulhasansakib.com
  * Text Domain: order-shield
@@ -23,7 +23,7 @@ if (!defined('ABSPATH')) {
 }
 
 // Define Plugin Constants
-define('OS_VERSION', '1.0.9');
+define('OS_VERSION', '1.1.0');
 define('OS_PLUGIN_FILE', __FILE__);
 define('OS_PLUGIN_DIR', plugin_dir_path(__FILE__));
 define('OS_PLUGIN_URL', plugin_dir_url(__FILE__));
